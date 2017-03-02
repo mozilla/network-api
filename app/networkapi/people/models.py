eb1064480b8da685490151beb6ef3cd34fe79fde
Christopher De Cairos <chris@chrisdecairos.ca>
2017-03-02 17:06:40 -0500

    Add People, Team and Link models

--- app/networkapi/people/models.py
+++ app/networkapi/people/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name: models.CharField(max_length=120)
+    role: models.CharField(max_length=120)
+    location: models.CharField(max_length=120)
+    teams: models.ManyToManyField(Team)
+    links: models.ManyToManyField(Link)
+    image: models.CharField(max_length=4096)
+    featured: models.BooleanField(default=False)
+    
+
+class Team(models.Model):
+    name: models.CharField(max_length=120)
+
+class Link(models.Model):
+    url: models.CharField(max_length=4096)
