e461d4da9c6037107499d308309142698f9188f9
Christopher De Cairos <chris@chrisdecairos.ca>
2017-03-03 11:27:42 -0500

    equals, not colon!

--- app/networkapi/people/models.py
+++ app/networkapi/people/models.py
@@ -4,22 +4,22 @@ class Person(models.Model):
     """
     A member of the Network
     """
-    name: models.CharField(max_length=120)
-    role: models.CharField(max_length=120)
-    location: models.CharField(max_length=120)
-    teams: models.ManyToManyField(Team)
-    links: models.ManyToManyField(Link)
-    image: models.CharField(max_length=4096)
+    name = models.CharField(max_length=120)
+    role = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    teams = models.ManyToManyField(Team)
+    links = models.ManyToManyField(Link)
+    image = models.CharField(max_length=4096)
 
     class Meta:
-        verbose_name_plural: 'people'
+        verbose_name_plural = 'people'
 
     def __str__(self):
         return str(self.name)
 
 
 class Team(models.Model):
-    name: models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
 
 class Link(models.Model):
-    url: models.CharField(max_length=4096)
+    url = models.CharField(max_length=4096)
