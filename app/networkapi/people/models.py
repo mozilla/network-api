3c96c0d83ebbce38093fe7b5222e86a3f848d086
Christopher DeCairos <christopherd@mozillafoundation.org>
2017-03-03 13:27:45 -0500

    Add the People, Team and Link models (#25)
    * Add People, Team and Link models
    
    * Alter the people model slightly
    
    * equals, not colon!
    
    * minor changes to Team and Link, makemigrations

--- app/networkapi/people/models.py
+++ app/networkapi/people/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return str(self.name);
+
+
+class Link(models.Model):
+    url = models.CharField(max_length=4096)
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return str(self.url);
+
+
+class Person(models.Model):
+    """
+    A member of the Network
+    """
+    name = models.CharField(max_length=300)
+    role = models.CharField(max_length=300)
+    location = models.CharField(max_length=300)
+    image = models.URLField(max_length=4096)
+    teams = models.ManyToManyField(
+        Team,
+        related_name='people',
+    )
+    links = models.ManyToManyField(
+        Link,
+        related_name='people',
+    )
+
+    class Meta:
+        verbose_name_plural = 'people'
+
+    def __str__(self):
+        return str(self.name)
