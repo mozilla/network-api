74dd30c29943e2193688dbf34dab264ef18f27ac
Christopher De Cairos <chris@chrisdecairos.ca>
2017-03-03 10:58:43 -0500

    Alter the people model slightly

--- app/networkapi/people/models.py
+++ app/networkapi/people/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 class Person(models.Model):
+    """
+    A member of the Network
+    """
     name: models.CharField(max_length=120)
     role: models.CharField(max_length=120)
     location: models.CharField(max_length=120)
     teams: models.ManyToManyField(Team)
     links: models.ManyToManyField(Link)
     image: models.CharField(max_length=4096)
-    featured: models.BooleanField(default=False)
-    
+
+    class Meta:
+        verbose_name_plural: 'people'
+
+    def __str__(self):
+        return str(self.name)
+
 
 class Team(models.Model):
     name: models.CharField(max_length=120)
