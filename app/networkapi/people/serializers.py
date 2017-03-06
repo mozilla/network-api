e098d329e7d1dc5c33a31413eba8c4581458d74e
Christopher De Cairos <chris@chrisdecairos.ca>
2017-03-06 15:03:08 -0500

    Implement Person serializer, views, and urls

---
+++ app/networkapi/people/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+from networkapi.people.models import (
+    Person,
+    Link,
+    Team,
+)
+
+class LinkSerializer(serializers.ModelSerializer):
+    """
+    Serializes a Link Model
+    """
+    class Meta:
+        model = Link
+        fields = (
+            'url',
+            'name',
+        )
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    """
+    Serializes a Team Model
+    """
+    class Meta:
+        model = Team
+        fields = (
+            'name',
+        )
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    """
+    Serializes a Person with Links and Teams included
+    """
+    teams = TeamSerializer(many=True)
+    links = LinkSerializer(many=True)
+    class Meta:
+        model = Person
+        fields = (
+            'name',
+            'role',
+            'location',
+            'image'
+        )
--- app/networkapi/people/views.py
+++ app/networkapi/people/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from networkapi.people.models import Person
+from networkapi.people.serializers import PersonSerializer
 
-# Create your views here.
+
+class PeopleListView(ListAPIView):
+    """
+    A view that permits a GET to allow listing all the People
+    in the database
+
+    **Route** - `/people`
+    """
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    pagination_class = None
+
+
+class PersonView(RetrieveAPIView):
+    """
+    A view that permits a GET to allow listing a person
+    in the database
+
+    **Route** - `/people/:id`
+    """
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    pagination_class = None
