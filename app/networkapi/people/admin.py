4376539266bb777eaafba255d43a556f01d00977
Gideon Thomas <gideon@mozillafoundation.org>
2017-03-13 18:26:59 -0400

    Remove redundant code

--- app/networkapi/people/admin.py
+++ app/networkapi/people/admin.py
@@ -11,19 +11,6 @@ from networkapi.people.forms import PersonAdminForm
 
 
 class PersonAdmin(SortableAdmin):
-    fields = (
-        'name',
-        'role',
-        'location',
-        'bio',
-        'image',
-        'partnership_logo',
-        'affiliations',
-        'links',
-        'internet_health_issues',
-        'featured',
-        'quote',
-    )
     form = PersonAdminForm
     sortable_change_list_template = (
         'people/adminsortable_change_list_custom.html'
