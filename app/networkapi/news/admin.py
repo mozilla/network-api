7dc5860d8fe9cd3ad0cf80a8d21474dc36d48287
Gideon Thomas <gideon@mozillafoundation.org>
2017-03-22 14:53:28 -0400

    Fix #66 - Expose News via the API

--- app/networkapi/news/admin.py
+++ app/networkapi/news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from networkapi.news.models import News
+from networkapi.news.models import News, Topic
 
 
 admin.site.register(News)
+admin.site.register(Topic)
--- app/networkapi/news/serializers.py
+++ app/networkapi/news/serializers.py
@@ -3,6 +3,22 @@ from rest_framework import serializers
 from networkapi.news.models import News
 
 
-class OpportunitySerializer(serializers.ModelSerializer):
+class NewsSerializer(serializers.ModelSerializer):
+    """
+    Serializes a News object
+    """
+    topic = serializers.StringRelatedField()
+
     class Meta:
         model = News
+        fields = (
+            'headline',
+            'outlet',
+            'topic',
+            'date',
+            'link',
+            'author',
+            'glyph',
+            'topic',
+            'featured',
+        )
