ce8933f884b4f7237ef9d9a2787abffa8518163c
Christopher De Cairos <chris@chrisdecairos.ca>
2017-03-28 16:03:04 -0400

    clean up settings.py

--- app/networkapi/settings.py
+++ app/networkapi/settings.py
@@ -35,12 +35,12 @@ BASE_DIR = root()
 
 APP_DIR = app()
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
+
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
