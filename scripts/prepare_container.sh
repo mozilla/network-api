758a5d7f539030cf21cf4387dd121f8fd6f71531
Christopher De Cairos <chris@chrisdecairos.ca>
2017-04-06 17:18:24 -0400

    remove old code from prepare_container.sh

--- scripts/prepare_container.sh
+++ scripts/prepare_container.sh
@@ -3,6 +3,3 @@
 mkdir -p media/images/shared
 
 cp networkapi/static/shared/images/default.png media/images/shared
-
-# Uninstall the fancy-shmancy media browser that doesn't work well with cloud based storage.
-# pip uninstall filebrowser_safe --yes
