417c900dbdf916543d0ec5dc2b3496179fb61c84
Pomax <pomax@nihongoresources.com>
2017-04-28 11:39:03 -0700

    Tabs

--- app/networkapi/utility/userpermissions.py
+++ app/networkapi/utility/userpermissions.py
@@ -1,33 +1,33 @@
 def ismoz(email):
-	"""
-	This function determines whether a particular email address is a
-	mozilla address or not. We strictly control mozilla.com and
-	mozillafoundation.org addresses, and so filtering on the email
-	domain section using exact string matching is acceptable.
-	"""
-	if email is None:
-		return False
+    """
+    This function determines whether a particular email address is a
+    mozilla address or not. We strictly control mozilla.com and
+    mozillafoundation.org addresses, and so filtering on the email
+    domain section using exact string matching is acceptable.
+    """
+    if email is None:
+        return False
 
-	parts = email.split('@')
-	domain = parts[1]
+    parts = email.split('@')
+    domain = parts[1]
 
-	if domain == 'mozilla.com':
-		return True
+    if domain == 'mozilla.com':
+        return True
 
-	if domain == 'mozillafoundation.org':
-		return True
+    if domain == 'mozillafoundation.org':
+        return True
 
-	return False
+    return False
 
 
 def set_user_permissions(backend, user, response, *args, **kwargs):
-	"""
-	This is a social-auth pipeline function for automatically
-	setting is_staff permissions when a user logs in from a 
-	known-to-be mozilla account.
-	"""
+    """
+    This is a social-auth pipeline function for automatically
+    setting is_staff permissions when a user logs in from a 
+    known-to-be mozilla account.
+    """
 
-	if user.email and ismoz(user.email):
-		if user.is_staff == False:
-			user.is_staff = True
-			user.save()
+    if user.email and ismoz(user.email):
+        if user.is_staff == False:
+            user.is_staff = True
+            user.save()
