689f3861fecada7ac821c54fadb3354636eea385
Pomax <pomax@nihongoresources.com>
2017-06-05 15:42:42 -0700

    flake8~~~~

--- app/pulseapi/entries/urls.py
+++ app/pulseapi/entries/urls.py
@@ -13,7 +13,15 @@ urlpatterns = [
     url('^$', EntriesListView.as_view(), name='entries-list'),
     url('bookmarks/', BookmarkedEntries.as_view(), name='user-bookmarks'),
     url(r'^(?P<entryid>[0-9]+)/bookmark/?', toggle_bookmark, name='bookmark'),
-    url(r'^(?P<entryid>[0-9]+)/moderate/(?P<stateid>[0-9]+)/?', toggle_moderation, name='moderate'),
+    url(
+    	r'^(?P<entryid>[0-9]+)/moderate/(?P<stateid>[0-9]+)/?',
+    	toggle_moderation,
+    	name='moderate'
+    ),
     url(r'^(?P<pk>[0-9]+)/', EntryView.as_view(), name='entry'),
-    url('moderation-states/', ModerationStateView.as_view(), name='moderation-states'),
+    url(
+    	'moderation-states/',
+    	ModerationStateView.as_view(),
+    	name='moderation-states'
+    ),
 ]
