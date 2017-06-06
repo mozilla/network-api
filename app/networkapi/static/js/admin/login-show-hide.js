63e85d8002b66ed523fbd22c972a82e6f8b1b7f7
Mike Kamermans <pomax@nihongoresources.com>
2017-06-06 08:54:51 -0700

    Update login-show-hide.js

--- app/networkapi/static/js/admin/login-show-hide.js
+++ app/networkapi/static/js/admin/login-show-hide.js
@@ -1,9 +1,11 @@
 const google = document.querySelector('form div.social-login');
 const admin = document.querySelector('div.admin-access');
 
-const adminToggle = document.getElementById('admin-login-button');
-adminToggle.addEventListener( "click", function(evt) {
-  [google, admin].forEach( function(e) {
-  	e.classList.toggle("hidden");
+const toggles = Array.from(document.querySelectorAll('admin-login-toggle'));
+toggles.forEach(function(toggle) {
+  toggle.addEventListener( "click", function(evt) {
+    [google, admin].forEach( function(e) {
+      e.classList.toggle("hidden");
+    });
   });
 });
