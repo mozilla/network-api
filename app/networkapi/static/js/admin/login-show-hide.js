ea6343d6664c5387fd1332860862aec373eb459c
Pomax <pomax@nihongoresources.com>
2017-06-06 09:39:30 -0700

    PR fixes

--- app/networkapi/static/js/admin/login-show-hide.js
+++ app/networkapi/static/js/admin/login-show-hide.js
@@ -1,11 +1,10 @@
-const google = document.querySelector('form div.social-login');
 const admin = document.querySelector('div.admin-access');
+const google = document.querySelector('form div.social-login');
 
-const toggles = Array.from(document.querySelectorAll('admin-login-toggle'));
-toggles.forEach(function(toggle) {
-  toggle.addEventListener( "click", function(evt) {
-    [google, admin].forEach( function(e) {
-      e.classList.toggle("hidden");
-    });
+const toggle = document.querySelector('button.admin-login');
+toggle.addEventListener( "click", function(evt) {
+  [admin, google].forEach( function(e) {
+    e.classList.toggle("hidden");
   });
 });
+
